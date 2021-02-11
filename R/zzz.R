71122e8dbc2f08c9c1a3910e7df770a80d216786
mb706 <mlr.developer@mb706.com>
2021-02-11 23:11:07 +0100

    add terminators to dictionaries

--- R/zzz.R
+++ R/zzz.R
@@ -12,6 +12,10 @@ lg = NULL
   mlr_optimizers = utils::getFromNamespace("mlr_optimizers", ns = "bbotk")
   mlr_optimizers$add("mies", OptimizerMies)
 
+  mlr_terminators = utils::getFromNamespace("mlr_terminators", ns = "bbotk")
+  mlr_terminators$add("gens", TerminatorGenerations)
+  mlr_terminators$add("budget", TerminatorBudget)
+
   assign("lg", lgr::get_logger("bbotk"), envir = parent.env(environment()))
 
 }
