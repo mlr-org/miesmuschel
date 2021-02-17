aac9023df8e92c32031a22f5fc64b2572b1cb3c9
mb706 <mlr.developer@mb706.com>
2021-02-17 22:12:06 +0100

    tests with bbotk loaded

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,4 +1,6 @@
 
 if (requireNamespace("tinytest", quietly = TRUE)) {
+  library("bbotk")
+  library("mlr3tuning")
   tinytest::test_package("miesmuschel", at_home = identical(Sys.getenv("NOT_CRAN"), "true"), ncpu = 2)
 }
