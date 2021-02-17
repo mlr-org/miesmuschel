bd04b62ff3a91fe5d768a4072367dc41babe64cc
mb706 <mlr.developer@mb706.com>
2021-02-17 22:28:33 +0100

    doc fix

--- R/TunerMies.R
+++ R/TunerMies.R
@@ -10,7 +10,7 @@ TunerMies = R6Class("TunerMies", inherit = mlr3tuning::TunerFromOptimizer,
     #' @param parent_selector ([`Selector`])
     #' @param survival_selector ([`Selector`])
     #' @param elite_selector ([`Selector`] | `NULL`)
-    #' @param multi_fidelity ([`logical(1)`])
+    #' @param multi_fidelity (`logical(1)`)
     initialize = function(mutator = MutatorProxy$new(), recombinator = RecombinatorProxy$new(), parent_selector = SelectorProxy$new(),
                           survival_selector = SelectorProxy$new(), elite_selector = NULL, multi_fidelity = FALSE) {
       super$initialize(OptimizerMies$new(mutator = mutator, recombinator = recombinator, parent_selector = parent_selector,
