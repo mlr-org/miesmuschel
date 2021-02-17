9fdc1b3e64d8ad3d5266f0b8895adc1a946ca9d1
mb706 <mlr.developer@mb706.com>
2021-02-17 22:07:21 +0100

    work without mlr3tuning

--- R/zzz.R
+++ R/zzz.R
@@ -12,12 +12,15 @@ lg = NULL
   mlr_optimizers = utils::getFromNamespace("mlr_optimizers", ns = "bbotk")
   mlr_optimizers$add("mies", OptimizerMies)
 
+
   mlr_terminators = utils::getFromNamespace("mlr_terminators", ns = "bbotk")
   mlr_terminators$add("gens", TerminatorGenerations)
   mlr_terminators$add("budget", TerminatorBudget)
 
-  mlr_tuners = utils::getFromNamespace("mlr_tuners", ns = "mlr3tuning")
-  mlr_tuners$add("mies", TunerMies)
+  if (requireNamespace("mlr3tuning", quietly = TRUE)) {
+    mlr_tuners = utils::getFromNamespace("mlr_tuners", ns = "mlr3tuning")
+    mlr_tuners$add("mies", TunerMies)
+  }
 
   assign("lg", lgr::get_logger("bbotk"), envir = parent.env(environment()))
 
