cca0e287300b5e66ce989f24df4c25f0debcc128
mb706 <mlr.developer@mb706.com>
2021-02-17 22:43:58 +0100

    load data.table for tests

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -2,5 +2,6 @@
 if (requireNamespace("tinytest", quietly = TRUE)) {
   library("bbotk")
   library("mlr3tuning")
+  library("data.table")
   tinytest::test_package("miesmuschel", at_home = identical(Sys.getenv("NOT_CRAN"), "true"), ncpu = 2)
 }
