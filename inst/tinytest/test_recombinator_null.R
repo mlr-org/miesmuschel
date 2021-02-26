4c2e808adafedc6f5f622badb61e8dd0f83d6680
mb706 <mlr.developer@mb706.com>
2021-02-26 23:26:20 +0100

    test null-recombinator's different n_indivs_in/out

--- inst/tinytest/test_recombinator_null.R
+++ inst/tinytest/test_recombinator_null.R
@@ -9,3 +9,24 @@ p = ps(ParamLgl = p_lgl(), ParamDbl = p_dbl(0, 1), ParamInt = p_int(0, 1), Param
 rnull$prime(p)
 vals = generate_design_random(p, 10)$data
 expect_identical(vals, rnull$operate(vals))
+expect_equal(rnull$n_indivs_in, 1)
+expect_equal(rnull$n_indivs_out, 1)
+
+rnull2 = RecombinatorNull$new(n_indivs_in = 2)
+rnull2$prime(p)
+vals = generate_design_random(p, 10)$data
+expect_identical(vals, rnull2$operate(vals))
+
+vals = generate_design_random(p, 9)$data
+expect_error(rnull2$operate(vals), "%%.* == 0.*Must be TRUE")
+
+expect_equal(rnull2$n_indivs_in, 2)
+expect_equal(rnull2$n_indivs_out, 2)
+
+rnull21 = RecombinatorNull$new(n_indivs_in = 2, n_indivs_out = 1)
+rnull21$prime(p)
+vals = generate_design_random(p, 10)$data
+expect_identical(vals[(1:5) * 2 - 1], rnull21$operate(vals))
+
+expect_equal(rnull21$n_indivs_in, 2)
+expect_equal(rnull21$n_indivs_out, 1)
