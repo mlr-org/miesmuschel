726301bbb1b43a66b96418141feb47e2b1ec8b51
mb706 <mlr.developer@mb706.com>
2021-03-02 22:19:37 +0100

    test TerminatorBudget

---
+++ inst/tinytest/test_TerminatorBudget.R
@@ -0,0 +1,58 @@
+
+tb = TerminatorBudget$new()
+expect_equal(tb$param_set$values, list(budget = Inf, aggregate = sum))
+
+oibig = as_oi(get_objective_passthrough("minimize", FALSE, "bud"))
+
+expect_error(tb$is_terminated(oibig), "Need exactly one budget parameter")
+
+oibig$search_space$params$bud$tags = "budget"
+
+expect_false(tb$is_terminated(oibig$archive))
+
+expect_equal(tb$status(oibig$archive), c(max_steps = 100, current_steps = 0))
+
+tb$param_set$values$budget = 0
+
+expect_true(tb$is_terminated(oibig$archive))
+expect_equal(tb$status(oibig$archive), c(max_steps = 100, current_steps = 100))
+
+design = cbind(generate_design_random(oibig$search_space, 9)$data[, bud := c(1, 1, 1, 3, 3, 7, 5, 5, 9)],
+  data.table(additional = 1:9, dob = rep(1:3, each = 3), eol = rep(c(3, NA, NA), 3))
+)
+oibig$eval_batch(design)
+
+tb$param_set$values$budget = 36
+expect_false(tb$is_terminated(oibig$archive))
+expect_equal(tb$status(oibig$archive), c(max_steps = 100, current_steps = floor(35 / 36 * 100)))
+
+oibig$eval_batch(design[1])
+
+expect_true(tb$is_terminated(oibig$archive))
+expect_equal(tb$status(oibig$archive), c(max_steps = 100, current_steps = 100))
+
+oibig$eval_batch(design[1])
+
+expect_true(tb$is_terminated(oibig$archive))
+expect_equal(tb$status(oibig$archive), c(max_steps = 100, current_steps = floor(37 / 36 * 100)))
+
+expect_error(suppressWarnings({tb$param_set$values$aggregate = max}), "must be a function.* finite numeric value")
+
+tb$param_set$values$aggregate = function(x) max(x, 0)
+tb$param_set$values$budget = 10
+
+expect_false(tb$is_terminated(oibig$archive))
+expect_equal(tb$status(oibig$archive), c(max_steps = 100, current_steps = floor(9 / 10 * 100)))
+
+oibig$eval_batch(design[1][, bud := 10])
+
+expect_true(tb$is_terminated(oibig$archive))
+expect_equal(tb$status(oibig$archive), c(max_steps = 100, current_steps = 100))
+
+
+# multi objective
+oim = as_oi(get_objective_passthrough(c("minimize", "maximize"), FALSE, "bud"))
+oim$search_space$params$bud$tags = "budget"
+expect_false(tb$is_terminated(oim$archive))
+oibig$eval_batch(design[, p2 := 1:9][9, bud := 10])
+expect_true(tb$is_terminated(oibig$archive))
