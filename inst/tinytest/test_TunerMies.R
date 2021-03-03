9452b523fc7fec7380cafc8bd5b683eba5021c4f
mb706 <mlr.developer@mb706.com>
2021-03-03 19:10:27 +0100

    testing TunerMies

---
+++ inst/tinytest/test_TunerMies.R
@@ -0,0 +1,25 @@
+
+source("setup.R", local = TRUE)
+
+learner = mlr3::lrn("classif.rpart")
+
+learner$param_set$values[c("cp", "maxdepth")] = list(to_tune())
+
+ti = mlr3tuning::TuningInstanceSingleCrit$new(
+  task = mlr3::tsk("iris"),
+  learner = learner,
+  resampling = mlr3::rsmp("holdout"),
+  measure = mlr3::msr("classif.acc"),
+  terminator = bbotk::trm("gens", generations = 10)
+)
+
+mies_tune <- mlr3tuning::tnr("mies", mutator = MutatorGauss$new(), recombinator = RecombinatorCrossoverUniform$new(),
+  parent_selector = SelectorRandom$new(), survival_selector = SelectorBest$new(),
+  mu = 10, lambda = 5
+)
+
+set.seed(1)
+mies_tune$optimize(ti)
+
+expect_set_equal(names(ti$result_x_domain), c("cp", "maxdepth"))
+expect_true(ti$result$classif.acc > 0.9)
