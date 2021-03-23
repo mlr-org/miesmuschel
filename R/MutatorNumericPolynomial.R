250e4136f39ec32f51a3bf9b777078635cadef96
sumny <lennart.sch@web.de>
2021-03-23 13:15:30 +0100

    drop maxmut for lower upper delta

--- R/MutatorNumericPolynomial.R
+++ R/MutatorNumericPolynomial.R
@@ -7,20 +7,14 @@
 #' @description
 #' Numeric components are mutated by sampling from a polynomial distribution having its mean at the
 #' current value and its variance as a function of the distribution index `n`.
+#' See `r cite_bib("deb1996combined")` for more details.
 #'
 #' Since the information loss is rather high, this should in most cases be combined with [`MutatorCmpMaybe`].
 #'
 #' @section Configuration Parameters:
-#' * `max_mut` :: `numeric`\cr
-#'  Maximum permissible perturbance allowed for each individual.
-#'  See `r cite_bib("deb1996combined")` for more details.
-#'  This may either be a scalar in which case it is applied to all input components, or a vector,
-#'  in which case it must have the length of the input and applies to components in order in which
-#'  they appear in the priming [`ParamSet`][paradox::ParamSet]. Initialized to 1.
-#' *  `n` :: `integer()`\cr
-#'  Distribution index of the polynomial distribution for each individual.
+#' *  `n` :: `double`\cr
+#'  Non-negative distribution index of the polynomial distribution for each individual.
 #'  Generally spoken, the higher `n`, the lower the variance of the distribution.
-#'  See `r cite_bib("deb1996combined")` for more details.
 #'  This may either be a scalar in which case it is applied to all input components, or a vector,
 #'  in which case it must have the length of the input and applies to components in order in which
 #'  they appear in the priming [`ParamSet`][paradox::ParamSet]. Initialized to 1.
@@ -50,41 +44,35 @@ MutatorNumericPolynomial = R6Class("MutatorNumericPolynomial",
     #' @description
     #' Initialize the `MutatorNumericPolynomial` object.
     initialize = function() {
-      param_set = ps(
-        max_mut = p_uty(custom_check = crate(function(x) {
-          check_numeric(x, lower = tol_bound(0, "lower"), any.missing = FALSE, min.len = 1L)
-        }, .parent = topenv()), tags = "required"),
-        n = p_uty(custom_check = crate(function(x) {
-          check_integerish(x, lower = 0L, any.missing = FALSE, min.len = 1L)
+      param_set = ps(n = p_uty(custom_check = crate(function(x) {
+          check_double(x, lower = 0L, any.missing = FALSE, min.len = 1L)
         }, .parent = topenv()), tags = "required")
       )
-      param_set$values = list(max_mut = 1, n = 1L)
-      super$initialize("ParamDbl", param_set)
+      param_set$values = list(n = 1)
+      super$initialize("ParamDbl", param_set = param_set)
     }
   ),
   private = list(
     .mutate_numeric = function(values, lowers, uppers) {
       n_values = length(values)
       params = self$param_set$get_values()
+      n = params$n
 
-      max_mut = pmax(params$max_mut, 0)
-      if (length(max_mut) %nin% c(1L, n_values)) {
-        stop("max_mut must have either length 1, or length of input.")
+      if (length(n) == 1L) {
+        n = rep(n, n_values)  # make the ifelse() further down work
       }
-
-      exponent = 1 / (params$n + 1)
-      if (length(exponent) == 1L) {
-        exponent = rep(exponent, n_values)  # make the ifelse() further down work
-      }
-      if (length(exponent) != n_values) {
+      if (length(n) != n_values) {
         stop("n must have either length 1, or length of input.")
       }
+      exponent = 1 / (n + 1)
 
-      u = stats::runif(n_values, min = 0, max = 1)
-      delta = ifelse(u < 0.5,
-        yes = ((2 * u) ^ exponent) - 1,
-        no = 1 - ((2 * (1 - u)) ^ exponent))
-      pmax(pmin(values + delta * max_mut, uppers), lowers)
+
+      delta = uppers - lowers
+      r = stats::runif(n_values, min = 0, max = 1)
+      deltaq = ifelse(r <= 0.5,
+        yes = ((2 * r + (1 - 2 * r) * (1 - (values - lowers) / delta) ^ (n + 1)) ^ exponent) - 1,
+        no = 1 - (2 * (1 - r) + 2 * (r - 0.5) * (1 - (uppers - values) / delta) ^ (n + 1)) ^ exponent)
+      pmax(pmin(values + deltaq * delta, uppers), lowers)
     }
   )
 )
--- inst/tinytest/test_mutator_numericpoly.R
+++ inst/tinytest/test_mutator_numericpoly.R
@@ -9,10 +9,6 @@ p = ps(x = p_dbl(0, 10))
 mpoly$prime(p)
 
 set.seed(1)
-# max_mut limits perturbance
-mpoly$param_set$values = list(max_mut = 3, n = 0)
-x_n0_m3 = mpoly$operate(data.table(x = rep(5, 100)))$x
-expect_true(all(abs(x_n0_m3 - 5) <= 3))
 
 # the higher the n the lower the variance
 mpoly$param_set$values$n = 0
@@ -21,31 +17,23 @@ mpoly$param_set$values$n = 4
 x_n4 = mpoly$operate(data.table(x = rep(5, 100)))$x
 expect_true(var(x_n0) > var(x_n4))
 
-# vector-valued max_mut
-mpoly$param_set$values = list(max_mut = c(1, 5), n = 0)
-mpoly$prime(ps(x = p_dbl(0, 10), y = p_dbl(0, 10)))
-operated = mpoly$operate(data.table(x = rep(5, 100), y = rep(5, 100)))
-expect_true(all(abs(operated$x - 5) <= 1))
-expect_true(all(abs(operated$x - 5) <= 5))
-expect_true(diff(range(operated$x)) < diff(range(operated$y)))
-
 # vector-valued n
-mpoly$param_set$values = list(max_mut = 1, n = c(0, 4))
+mpoly$param_set$values = list(n = c(0, 4))
 mpoly$prime(ps(x = p_dbl(0, 2), y = p_dbl(0, 2)))
 operated = mpoly$operate(data.table(x = rep(1, 100), y = rep(1, 100)))
 expect_true(var(operated$x) > var(operated$y))
 
 # test that two dimensions are treated independently
-mpoly$param_set$values = list(max_mut = 1, n = 0)
+mpoly$param_set$values = list(n = 0)
 mpoly$prime(ps(x = p_dbl(-2, 2), y = p_dbl(-2, 2)))
 operated = mpoly$operate(data.table(x = rep(0, 100), y = rep(0, 100)))
 expect_true(operated[, cor.test(x, y)]$p.value >= .05)
 
 # test integer
-mpoly$param_set$values = list(max_mut = 10, n = 0)
+mpoly$param_set$values = list(n = 0)  # n = 0 is unif
 mpoly$prime(ps(x = p_int(1, 5)))
-operated = mpoly$operate(data.table(x = rep(3, 100)))
+operated = mpoly$operate(data.table(x = rep(3, 1000)))
 expect_true(all(operated$x >= 1) && all(operated$x <= 5))
-expect_true(mean(operated$x %in% c(1, 5)) > .5)
-
+emp_ecdf = ecdf(operated$x)
+expect_true(all(abs(emp_ecdf(1:5) - c(0.2, 0.4, 0.6, 0.8, 1)) < 0.05))
 
