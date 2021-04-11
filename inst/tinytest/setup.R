db7dcaf0ba61704228bf40843468a7ad0cc4dac4
mb706 <mlr.developer@mb706.com>
2021-04-11 21:04:34 +0200

    even less logging

--- inst/tinytest/setup.R
+++ inst/tinytest/setup.R
@@ -4,7 +4,10 @@ checkmate::register_test_backend("tinytest")
 
 library("data.table")
 loadNamespace("mlr3")
+loadNamespace("bbotk")
+
 lgr::get_logger("mlr3")$set_threshold("warn")
+lgr::get_logger("bbotk")$set_threshold("warn")
 
 for (helper in list.files(pattern = "^helper_.*\\.R$")) {
   source(helper, local = TRUE)
