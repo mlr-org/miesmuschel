188b2ad70e6e563ab3ee018fd8e94ceacbd5bc89
mb706 <mlr.developer@mb706.com>
2021-09-15 22:41:05 +0200

    ablation_prepare with random sampling

--- attic/experiments/ablation_prepare.R
+++ attic/experiments/ablation_prepare.R
@@ -18,7 +18,7 @@ rq.5a.tbl <- rbindlist(lambdas.2)
 rq.5a.tbl.cond <- rbindlist(lambdas.1)[,
   c("filter_factor_last", "filter_factor_last.end") := sqrt(filter_factor_last.end * filter_factor_last)][,
   c("filter_factor_first", "filter_factor_first.end") := sqrt(filter_factor_first.end * filter_factor_first)][,
-  c("filter_select_per_tournament", "filter_select_per_tournament.end") := round((filter_select_per_tournament.end + filter_select_per_tournament) / 2)][,
+  c("filter_select_per_tournament", "filter_select_per_tournament.end") := round(sqrt(filter_select_per_tournament.end * filter_select_per_tournament))][,
   c("random_interleave_fraction", "random_interleave_fraction.end") := (random_interleave_fraction + random_interleave_fraction.end) / 2][]
 
 # RQ 5b
@@ -39,6 +39,9 @@ t2 <- CJ(id = 1:2, surrogate_learner = c("knn1", "knn7", "bohblrn", "ranger"))
 
 surgrid <- t1[t2, on = "id"][, id := NULL][surrogate_learner != original_surrogate_learner][, original_surrogate_learner := NULL][]
 rigrid <- rbind(
+  rbindlist(canonical[siman & infillsearch == "all" & batchmethod == "smashy"]$lambda)[,
+    c("random_interleave_fraction", "random_interleave_fraction.end") := 1][,
+      sample := "random"],
   rbindlist(canonical[siman & infillsearch == "all" & batchmethod == "smashy"]$lambda)[,
     c("random_interleave_fraction", "random_interleave_fraction.end") := 1],
   rbindlist(canonical[siman & infillsearch == "all" & batchmethod == "smashy"]$lambda)[,
