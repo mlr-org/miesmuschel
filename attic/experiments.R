6ae390c4f519865424c30c455576ffaf33e00f3e
mb706 <mlr.developer@mb706.com>
2022-07-02 23:27:03 +0200

    notes

--- attic/experiments.R
+++ attic/experiments.R
@@ -54,8 +54,8 @@ tinytest::test_all()
 
 tinytest::run_test_file("inst/tinytest/test_dictionaries.R")
 
-
-
+tinytest::run_test_file("inst/tinytest/test_mies_filter_surprog.R")    # TODO
+tinytest::run_test_file("inst/tinytest/test_mies_filter_offspring.R")  # TODO
 tinytest::run_test_file("inst/tinytest/test_mutator_proxy.R")       # ...
 tinytest::run_test_file("inst/tinytest/test_OptimizerSumoHB.R")     # properties are wrong
 tinytest::run_test_file("inst/tinytest/test_recombinator_proxy.R")  # ...
