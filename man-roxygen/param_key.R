13fe50ce4be8a4aac2ebebbb7f49cc5ac603ae3e
mb706 <mlr.developer@mb706.com>
2022-07-07 19:12:35 +0200

    document

---
+++ man-roxygen/param_key.R
@@ -0,0 +1,2 @@
+#' @param key (`character(1)`)\cr
+#'   The dictionary entry for which to display the help record.
--- man-roxygen/param_surrogate_selector.R
+++ man-roxygen/param_surrogate_selector.R
@@ -1,3 +1,3 @@
 #' @param surrogate_selector ([`Selector`])
-#'   Regression learner for the surrogate model filtering algorithm.\cr
+#'   [`Selector`] for the surrogate model filtering algorithm.\cr
 #'   The `$surrogate_selector` field will reflect this value.
