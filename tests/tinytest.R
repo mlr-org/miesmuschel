daacdf76f0837a5e4599909226baee3b46ca4d54
mb706 <mlr.developer@mb706.com>
2022-11-08 22:28:38 +0100

    sleep after finish to wait for parallel workers to quit

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,4 +1,5 @@
 
 if (requireNamespace("tinytest", quietly = TRUE)) {
   tinytest::test_package("miesmuschel", at_home = identical(Sys.getenv("NOT_CRAN"), "true"), ncpu = 2)
+  Sys.sleep(5)  # wait for parallel workers to quit
 }
