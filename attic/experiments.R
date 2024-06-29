9deccb685a2781b2a39b4e239ca8ae5a32b5fa2b
mb706 <mlr.developer@mb706.com>
2024-06-29 20:27:29 +0200

    news and document

--- attic/experiments.R
+++ attic/experiments.R
@@ -4,11 +4,10 @@ library("checkmate")
 library("data.table")
 
 
-
 devtools::document()
 devtools::load_all()
 
-devtools::run_examples(run_donttest = TRUE)
+devtools::run_examples(run_donttest = TRUE, document = FALSE)
 
 options(miesmuschel.testing = TRUE)
 
